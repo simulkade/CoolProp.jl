bd0bedc87e006a1b33e9fd28b0c7611d43d3f7d5
AA Eftekhari <e.eftekhari@gmail.com>
2016-11-06 09:29:57 +0100

    remove invalid entry from the test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using CoolProp
 using Base.Test
 @test (CoolProp.PropsSI("T","P",101325.0,"Q",0.0,"Water")-373.1242958476879)<1e-5
 # See https://github.com/CoolProp/CoolProp/issues/1293
-for param in ["version", "gitrevision", "errstring", "warnstring", "FluidsList", "incompressible_list_pure", "incompressible_list_solution", "mixture_binary_pairs_list", "parameter_list", "predefined_mixtures", "HOME", "cubic_fluids_schema", "invalid"]
+for param in ["version", "gitrevision", "errstring", "warnstring", "FluidsList", "incompressible_list_pure", "incompressible_list_solution", "mixture_binary_pairs_list", "parameter_list", "predefined_mixtures", "HOME", "cubic_fluids_schema"]
   println(param * " = " * get_global_param_string(param))
 end
